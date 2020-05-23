5c9a30aba7d779d7000d9d09a526c3208a84027a
blugnomeXD <52544829+blugnomeXD@users.noreply.github.com>
2020-05-23 21:29:09 +0200

    Update Login.php

--- coches/app/clases/Login.php
+++ coches/app/clases/Login.php
@@ -17,10 +17,8 @@
 
                 $sql = "SELECT id_usuario,usuario,tipo_usuario FROM usuario WHERE usuario = ? AND password = ?";
                 $stmt = $this->conn->prepare($sql);
-               
-                    $stmt = $this->conn->prepare($sql);
-                 /*
-                    $stmt->bind_param('ss',$nia,$password);
+                $stmt->bind_param('ss',$nia,$password);
+                /*
                     $stmt->bind_result($id,$usuario,$tipo_usuario);
                     $stmt->execute(); 
         
