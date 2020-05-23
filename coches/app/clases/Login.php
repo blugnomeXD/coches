d9d563b0ee30f7208793894392119602d0772ac0
blugnomeXD <52544829+blugnomeXD@users.noreply.github.com>
2020-05-23 21:34:48 +0200

    Update Login.php

--- coches/app/clases/Login.php
+++ coches/app/clases/Login.php
@@ -20,18 +20,15 @@
                 $stmt->bind_param('ss',$nia,$password); 
                 $stmt->bind_result($id,$usuario,$tipo_usuario);
                 $stmt->execute(); 
-        /*
+       
         
                     if($stmt->fetch()){  
                             session_start();  
                             $_SESSION['$usuario'] = $usuario;
                             $_SESSION['$tipo_usuario'] = $tipo_usuario;
                             $_SESSION['$id'] = $id;
-                    }else{
-        
-                        //header('Location: ../../index.html');
-                    } 
-                   
+                    }
+                   /* 
                 $stmt->close();
                 $this->conn->close();
                 */
