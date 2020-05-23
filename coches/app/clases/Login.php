e2d0f46d99e4a6b9eef5bb219a9c745bc96715d3
blugnomeXD <52544829+blugnomeXD@users.noreply.github.com>
2020-05-23 21:48:42 +0200

    Update Login.php

--- coches/app/clases/Login.php
+++ coches/app/clases/Login.php
@@ -14,23 +14,25 @@
             public function login($nia,$password){
 
                 $sql = "SELECT id_usuario,usuario,tipo_usuario FROM usuario WHERE usuario = ? AND password = ?";
-                $stmt = $this->conn->prepare($sql);
-                $stmt->bind_param('ss',$nia,$password); 
-                $stmt->bind_result($id,$usuario,$tipo_usuario);
-                $stmt->execute(); 
-       
+  
+                    $stmt = $this->conn->prepare($sql);
+                    $stmt->bind_param('ss',$nia,$password);
+                    $stmt->bind_result($id,$usuario,$tipo_usuario);
+                    $stmt->execute(); 
+        
         
                     if($stmt->fetch()){  
                             session_start();  
                             $_SESSION['$usuario'] = $usuario;
                             $_SESSION['$tipo_usuario'] = $tipo_usuario;
                             $_SESSION['$id'] = $id;
+                    }else{
+        
+                        //header('Location: ../../index.html');
                     }
                     
                 $stmt->close();
-             
                 $this->conn->close();
-                
              }  
 
         public function validarLogin($usuario){
