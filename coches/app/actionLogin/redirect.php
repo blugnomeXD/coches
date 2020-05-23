51a465316d882685b477259ccf6cde9c79041247
blugnomeXD <52544829+blugnomeXD@users.noreply.github.com>
2020-05-23 19:02:37 +0200

    Update redirect.php

--- coches/app/actionLogin/redirect.php
+++ coches/app/actionLogin/redirect.php
@@ -2,7 +2,9 @@
 session_start();
 require_once('../clases/Login.php');
 
-    
+    $login = new Login;
+
+        $login->login($user,$password);
         
           header("Location: https://www.marca.com/");
          exit();
